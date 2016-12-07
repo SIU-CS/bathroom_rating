8942cef76309b8b7a83dbabe733e80c88ef9ba78
arjunyel <arjunyel@gmail.com>
2016-12-07 11:42:21 -0600

    Firebase login is working

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -13,5 +13,6 @@ module.exports = {
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
-  navigateFallback: '/index.html'
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [ /^\/[^\_]+\/?/ ]
 };
